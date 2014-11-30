4b174b66adcdb0eab13dc72da0ea6ed214511789
Maarten Visscher <mail@maartenvisscher.nl>
2014-11-30 03:07:35 +0100

    Renamed AuthenticationException to TvAuthenticationException

--- src/main/java/net/afiake/samsungtvcontrol/TvAuthenticationException.java
+++ src/main/java/net/afiake/samsungtvcontrol/TvAuthenticationException.java
@@ -4,8 +4,8 @@ package net.afiake.samsungtvcontrol;
  * Thrown when the television denied access, we are not allowed to control the television. This happens when the television user denies access
  * or on timeout or cancel.
  */
-public class AuthenticationException extends Exception {
-    public AuthenticationException(String message) {
+public class TvAuthenticationException extends Exception {
+    public TvAuthenticationException(String message) {
         super(message);
     }
 }
