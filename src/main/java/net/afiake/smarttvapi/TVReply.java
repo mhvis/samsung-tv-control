4e94906108b1f4e009931bd20fe3cc901fd1bcb6
Maarten Visscher <mail@maartenvisscher.nl>
2015-01-30 16:21:32 +0100

    Total rewrite

---
+++ src/main/java/net/afiake/smarttvapi/TVReply.java
@@ -0,0 +1,22 @@
+package net.afiake.smarttvapi;
+
+/**
+ * TV response after authentication.
+ *
+ * @author Maarten Visscher
+ */
+public enum TVReply {
+
+    /**
+     * Authenticated, TV will respond to key codes.
+     */
+    ALLOWED,
+    /**
+     * We are not allowed to send key codes (TV user denied this controller).
+     */
+    DENIED,
+    /**
+     * Control request timed out or was canceled by the TV user.
+     */
+    TIMEOUT;
+}
